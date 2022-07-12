8d70c4f6e00a3dab25b7631dd91a58613e6fb1c2
manojyadavit92 <manojyadavit92@gmail.com>
2022-07-12 16:05:14 +0530

    category name with alfanumeric

--- application/views/admin/categories/edit-category.php
+++ application/views/admin/categories/edit-category.php
@@ -39,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<div class="form-group col-md-6 col-md-offset-3"></div>
                         <div class="form-group col-md-6">
                             <label for="email">Category Name<span>*</span> : </label>
-                            <input type="text" class="form-control" placeholder="Category Name" name="category_name" maxlength="50" value="<?= $editCategory[0]->category_name ?>" onKeyUp="this.value=this.value.replace(/[^A-z_ ]/g,'');" required>
+                            <input type="text" class="form-control" placeholder="Category Name" name="category_name" maxlength="50" value="<?= $editCategory[0]->category_name ?>"  required>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="destination">Select Parent Category : </label>                        
