3c1287d85d83ca3496076f227b6f9343040831c1
manojyadavit92 <manojyadavit92@gmail.com>
2022-09-07 16:01:57 +0530

    tracking link

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
  */
 //$config['base_url'] = 'http://localhost/project/royal-serry-dev/';
-$base = "https://" . $_SERVER['HTTP_HOST'];
+$base = "http://" . $_SERVER['HTTP_HOST'];
 $base .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $base;
 //$config['base_url'] = 'http://localhost/project/royal-serry-dev/';
